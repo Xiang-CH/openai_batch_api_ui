13eaa7b9ec6d9d52a6f5ef65ac4a97a6f98ad3e2
Xiang <cxiang@connect.hku.hk>
2024-08-23 10:43:59 +0800

    Added store/load template function and support for different response type

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import gradio as gr
 import dotenv
 import os
@@ -8,7 +9,6 @@ from openai import OpenAI
 from batch_apis import to_batch_jsonl, start_batch_job, get_batch_results, format_batch_results
 
 dotenv.load_dotenv()
-os.chdir(os.path.dirname(__file__))
 
 DEFAULT_MODEL = "gpt-4o"
 DEFAULT_OUTPUT_COLUMN = "gpt_response"
@@ -17,27 +17,24 @@ DEFAULT_TOP_P = 0.9
 
 URL = "/v1/chat/completions"
 
+os.chdir(os.path.dirname(__file__))
 if not os.path.exists("data"):
     os.mkdir("data")
-    os.mkdir("data/metas")
-    os.mkdir("data/output")
 
 file_path = {
+    "templates": "data/templates/",
     "jsonl": "data/metas/",
     "output": "data/output/"
 }
 
+for path in file_path.values():
+    if not os.path.exists(path):
+        os.mkdir(path)
+
 def save_api_key(api_key):
     dotenv.set_key(".env", "OPENAI_API_KEY", api_key)
 
-def get_params_from_prompt(sys_prompt, user_prompt):
-    prompt = sys_prompt + user_prompt
-    keys = re.findall(r'\{(\w+)\}', prompt)
-    if len(keys) == 0:
-        return "提示词中没有参数"
-    return ", ".join(keys)
-
-def get_params_from_prompt_list(prompt):
+def get_params_from_prompt_list(prompt): 
     keys = re.findall(r'\{(\w+)\}', prompt)
     return keys
 
@@ -51,8 +48,24 @@ def check_file(file):
     df = pd.read_csv(file) if file.name.endswith(".csv") else pd.read_excel(file)
     return [file, gr.Dataframe(df, label="原始数据表", interactive=False, visible=True)]
 
-def request_batch(api_key, model_name, temperature, top_p, frequency_penalty, presence_penalty, system_prompt, user_prompt, df, output_column):
+def validate_data(user_prompt, response_format, json_schema):
+    if user_prompt is None or user_prompt.strip() == "":
+        gr.Warning("请填写用户提示词(User Prompt)!", duration=5)
+        return False
+    if response_format == 'json_schema':
+        try:
+            json_schema = json.loads(json_schema)
+        except:
+            gr.Warning("请填写正确的json_schema!", duration=5)
+            return False
+    if response_format == 'json_object' and 'json' not in user_prompt.lower():
+        gr.Warning(message="使用json_object格式，User Promp必须包含关键词：json!", duration=5)
+        return False
+    return True
+
+def request_batch(api_key, model_name, temperature, top_p, frequency_penalty, presence_penalty, system_prompt, user_prompt, df, output_column, response_format, json_schema=None):
     # return "temp.xlsx"
+
     if not model_name:
         model_name = DEFAULT_MODEL
     if not output_column:
@@ -64,12 +77,14 @@ def request_batch(api_key, model_name, temperature, top_p, frequency_penalty, pr
     if df is None:
         gr.Warning("请上传文件!", duration=5)
         return
-    if system_prompt is None:
-        gr.Warning("请填写系统提示词(System Prompt)!", duration=5)
+    
+    if not validate_data(user_prompt, response_format, json_schema):
         return
-    if user_prompt is None:
-        gr.Warning("请填写用户提示词(User Prompt)!", duration=5)
-        return
+    
+    if response_format == 'json_schema':
+        json_schema = json.loads(json_schema)
+    else:
+        json_schema = None
 
     if output_column in df.columns:
         gr.Warning(f"输出参数名“{output_column}”已经在数据表中请检查!", duration=5)
@@ -104,27 +119,45 @@ def request_batch(api_key, model_name, temperature, top_p, frequency_penalty, pr
 
     file_name = f"{uuid.uuid4()}"
     jsonl_request_file = file_path['jsonl'] + file_name + '_req.jsonl'
-    if not to_batch_jsonl(df, jsonl_request_file, system_prompt, user_prompt, model_name, URL, options, json_mode=False):
+    if not to_batch_jsonl(df, jsonl_request_file, system_prompt, user_prompt, model_name, URL, options, response_format=response_format, json_schema=json_schema):
         return gr.Error(f"写入Jsonl文件失败")
+    gr.Info(f"请求Jsonl文件已存至: {jsonl_request_file}")
     batch_id = start_batch_job(jsonl_request_file, client)
     if not batch_id:
         return
     jsonl_response_file = file_path['jsonl'] + file_name + '_res.jsonl'
-    if not get_batch_results(batch_id, jsonl_response_file, client):
+    jsonl_response_file =  get_batch_results(batch_id, jsonl_response_file, client)
+    if not jsonl_response_file:
         return
     excel_file_out = file_path['output'] + file_name + '.xlsx'
-    if format_batch_results(df, jsonl_response_file, excel_file_out, output_column):
+    if format_batch_results(df, jsonl_response_file, excel_file_out, output_column, json_mode=response_format!="text"):
         return excel_file_out
     return
     
+def check_response_format(response_format):
+    if response_format != 'json_schema':
+        return gr.Row(visible=False)
+    return gr.Row(visible=True)
 
+def check_json(json_schema):
+    try:
+        json.loads(json_schema.strip())
+        return "解析成功"
+    except Exception as e:
+        return "解析失败: " + str(e)
 
+with gr.Blocks(theme=gr.themes.Default()) as demo:
+    update_by_code = gr.State(False)
 
-with gr.Blocks() as demo:
     gr.Markdown("## OpenAI 批处理 UI")
     with gr.Row():
-        api_key = gr.Textbox(label="OpenAI API Key", value=os.getenv("OPENAI_API_KEY"), placeholder="输入你的OpenAI API Key")
+        select_template = gr.Dropdown(label="选择模板", choices=["-"]+[f.split(".")[0] for f in os.listdir(file_path['templates'])], value="-", interactive=True, scale=4)
+        delete_current_template = gr.Button("删除当前模板", variant='stop', interactive=False)
+
+    with gr.Row():
+        api_key = gr.Textbox(label="OpenAI API Key", value=os.getenv("OPENAI_API_KEY"), placeholder="输入你的OpenAI API Key", scale=3)
         model_name = gr.Textbox(label="Model Name", placeholder=DEFAULT_MODEL)
+        response_format = gr.Dropdown(label="Response Format", choices=["text", "json_object", "json_schema"], value="text", interactive=True)
         api_key.change(save_api_key, inputs=api_key)
     
     with gr.Row():
@@ -133,28 +166,125 @@ with gr.Blocks() as demo:
         frequency_penalty = gr.Slider(label="Frequency Penalty", minimum=0.0, maximum=1.0, step=0.1, value=0.0, interactive=True)
         presence_penalty = gr.Slider(label="Presence Penalty", minimum=0.0, maximum=1.0, step=0.1, value=0.0, interactive=True)
 
+    with gr.Row(visible=(response_format == 'json_schema')) as jsons:
+        json_schema_code = gr.Code(language="json", label="json_schema", interactive=True, scale=2)
+        json_schema_parsed = gr.Textbox(label="json_schema解析状态", value="待填写", interactive=False)
+
+    json_schema_code.change(check_json, inputs=json_schema_code, outputs=json_schema_parsed)
+    response_format.change(check_response_format, inputs=response_format, outputs=jsons)
+
     with gr.Row():
+        with gr.Column(scale=2):
+            system_prompt = gr.Textbox(label="System Prompt", placeholder="输入系统提示词（用{ }表示参数）（选填）")
+            user_prompt = gr.Textbox(label="User Prompt", placeholder="输入用户提示词（用{ }表示参数）", lines=10)
         with gr.Column():
-            system_prompt = gr.Textbox(label="System Prompt", placeholder="输入系统提示词（用{ }表示参数）")
-            user_prompt = gr.Textbox(label="User Prompt", placeholder="输入用户提示词（用{ }表示参数）", lines=5)
-        with gr.Column():
-            params = gr.Textbox(label="输入参数", placeholder="在左侧输入提示词自动识别参数名", interactive=False, lines=5)
-            output_column = gr.Textbox(label="输出参数名", placeholder="gpt_response")
-        user_prompt.change(fn=get_params_from_prompt, inputs=[system_prompt, user_prompt], outputs=params)
-        system_prompt.change(fn=get_params_from_prompt, inputs=[system_prompt, user_prompt], outputs=params)
+            with gr.Blocks():
+                params = gr.Textbox(label="输入参数", placeholder="在左侧输入提示词自动识别参数名", interactive=False, lines=2)
+                output_column = gr.Textbox(label="GPT输出列表头", placeholder="gpt_response")
+            with gr.Column(visible=True) as save_template_col:
+                template_name = gr.Textbox(label="模板名", placeholder="输入模板名称（选填）", interactive=True)
+                save_template_btn = gr.Button("保存模板", variant='secondary')
+
 
     file_in = gr.File(label="上传原始数据表")
     df = gr.Dataframe(label="原始数据表", interactive=False, visible=False)
     file_in.upload(check_file, inputs=file_in, outputs=[file_in, df])
     file_in.clear(lambda x: None, inputs=file_in, outputs=df)
 
-    request_btn = gr.Button("请求批处理🚀")
+    request_btn = gr.Button("请求批处理🚀", variant='primary')
+    progress = gr.Progress(track_tqdm=True)
     file_out = gr.File(label="下载处理后数据表")
-    request_btn.click(request_batch, inputs=[api_key, model_name, temperature, top_p, frequency_penalty, presence_penalty, system_prompt, user_prompt, df,output_column], outputs=file_out)
+    request_btn.click(request_batch, inputs=[api_key, model_name, temperature, top_p, frequency_penalty, presence_penalty, system_prompt, user_prompt, df, output_column, response_format, json_schema_code], outputs=file_out)
 
-    # with gr.Row() as metas:
-    #     batch_id = gr.Textbox(label="Batch ID", placeholder="请求批处理任务后显示", interactive=False)
-    #     batch_id = gr.Textbox(label="Batch ID", placeholder="请求批处理任务后显示", interactive=False)
+    # 输入提示词后后显示参数
+    def get_params_from_prompt(sys_prompt, user_prompt):
+        prompt = sys_prompt + user_prompt
+        keys = re.findall(r'\{(\w+)\}', prompt)
+        if len(keys) == 0:
+            return "提示词中没有参数"
+        return ", ".join(keys)
+    user_prompt.change(fn=get_params_from_prompt, inputs=[system_prompt, user_prompt], outputs=params)
+    system_prompt.change(fn=get_params_from_prompt, inputs=[system_prompt, user_prompt], outputs=params)
+
+    # 删除模板
+    @delete_current_template.click(inputs=[select_template], outputs=[select_template, delete_current_template])
+    def delete_template(template):
+        if template == "-":
+            gr.Warning("请选择模板!", duration=5)
+            return
+        try:
+            os.remove(file_path['templates'] + f"{template}.json")
+            gr.Info(f"模板 {template} 已删除!")
+            return [gr.Dropdown(choices=["-"]+[f.split(".")[0] for f in os.listdir(file_path['templates'])], value="-"), gr.Button(interactive=False)]
+        except:
+            gr.Error(f"删除模板失败: {template}")
+            return
+
+    # 保存模板
+    @save_template_btn.click(inputs=[template_name, system_prompt, user_prompt, model_name, response_format, output_column, json_schema_code, temperature, top_p, frequency_penalty, presence_penalty, select_template], outputs=[save_template_col, select_template, delete_current_template])
+    def save_template(template_name, system_prompt, user_prompt, model_name, response_format, output_column, json_schema, temperature, top_p, frequency_penalty, presence_penalty, select_template):
+        if not model_name:
+            model_name = DEFAULT_MODEL
+        if not output_column:
+            output_column = DEFAULT_OUTPUT_COLUMN
+
+        if not template_name:
+            gr.Warning("请填写模板名!", duration=5)
+            return
+        
+        if template_name == "-":
+            gr.Warning("\"-\"不能作为模板名模板名!", duration=5)
+            return
+        
+        for existing_template in os.listdir(file_path['templates']):
+            if existing_template == f"{template_name}.json":
+                gr.Warning(f"模板名 {template_name} 已存在，请更换!", duration=5)
+                return
+        
+        if not validate_data(user_prompt, response_format, json_schema):
+            return
+        
+        template = {
+            "system_prompt": system_prompt,
+            "user_prompt": user_prompt,
+            "model_name": model_name,
+            "response_format": response_format,
+            "output_column": output_column,
+            "json_schema": json.loads(json_schema) if response_format == 'json_schema' else None,
+            "temperature": temperature,
+            "top_p": top_p,
+            "frequency_penalty": frequency_penalty,
+            "presence_penalty": presence_penalty
+        }
+        with open(file_path['templates'] + f"{template_name}.json", 'w') as f:
+            json.dump(template, f, ensure_ascii=False) 
+        gr.Info(f"模板已保存至: {file_path['templates']}{template_name}.json")
+
+        return [gr.Column(visible=False), gr.Dropdown(choices=["-"]+[f.split(".")[0] for f in os.listdir(file_path['templates'])], value=template_name), gr.Button(interactive=False)]
+
+    # 选择模板后自动填充参数
+    @select_template.input(inputs=[select_template], outputs=[system_prompt, user_prompt, model_name, response_format, output_column, json_schema_code, temperature, top_p, frequency_penalty, presence_penalty])
+    def load_template(template):
+        if template == "-":
+            return [None, None, None, None, None, None, None, None, None, None]
+        try:
+            with open(file_path['templates'] + f"{template}.json", 'r') as f:
+                template = json.load(f)
+        except:
+            gr.Error(f"读取模板失败: {template}")
+            return [None, None, None, None, None, None, None, None, None, None]
+        return [template['system_prompt'], template['user_prompt'], template['model_name'], template['response_format'], template['output_column'], json.dumps(template['json_schema']) if template['response_format'] == 'json_schema' else None, template['temperature'], template['top_p'], template['frequency_penalty'], template['presence_penalty'], True]
+
+    # 选择模板后启用删除按钮，隐藏保存模板栏;
+    @select_template.change(inputs=select_template, outputs=[delete_current_template, save_template_col])
+    def on_select_template(template):
+        if template == "-":
+            return [gr.update(interactive=False), gr.Column(visible=True)]
+        return [gr.update(interactive=True), gr.Column(visible=False)]
+
+    # 参数修改后隐藏删除按钮，显示保存模板栏；
+    for element in [system_prompt, user_prompt, model_name, response_format, output_column, json_schema_code, temperature, top_p, frequency_penalty, presence_penalty]:
+        element.input(lambda: gr.Dropdown(value="-"), outputs=select_template)
 
     
 
--- batch_apis.py
+++ batch_apis.py
@@ -1,42 +1,51 @@
 import time
 import json
-import os
 import pandas as pd
 import re
 import gradio as gr
 import numpy as np
-from openai import OpenAI
+import os
+from tqdm import tqdm
 
-os.chdir(os.path.dirname(__file__))
+current_dir = os.path.dirname(__file__)
 
 def get_params_from_prompt_list(prompt):
     keys = re.findall(r'\{(\w+)\}', prompt)
     return keys
 
 # jsonl 格式 batch 请求文件
-def to_batch_jsonl(df: pd.DataFrame, file_out: str, sys_prompt_template: str, user_prompt_template: str, model: str, url: str, options: object, json_mode: bool = False):
+def to_batch_jsonl(df: pd.DataFrame, file_out: str, sys_prompt_template: str, user_prompt_template: str, model: str, url: str, options: object, response_format: str, json_schema: str):
+    file_out = os.path.join(current_dir, file_out)
+    print("current_direcrtory", current_dir)
+    print("file_out", file_out)
     try:
         with open(file_out, 'w', encoding='utf-8') as f:
             for index, row in df.iterrows():
-
-                sys_args = {param: row[param] for param in get_params_from_prompt_list(sys_prompt_template)}
+                
                 user_args = {param: row[param] for param in get_params_from_prompt_list(user_prompt_template)}
-                prompt = [
-                    {"role": "system", "content": sys_prompt_template.format(**sys_args)},
-                    {"role": "user", "content": user_prompt_template.format(**user_args)}
-                ]
-                data = get_batch_line(id=index, messages=prompt, model=model, url=url, options=options, json_output=json_mode)
+                if sys_prompt_template: 
+                    sys_args = {param: row[param] for param in get_params_from_prompt_list(sys_prompt_template)}
+                    prompt = [
+                        {"role": "system", "content": sys_prompt_template.format(**sys_args)},
+                        {"role": "user", "content": user_prompt_template.format(**user_args)}
+                    ]
+                else: 
+                    prompt = [{"role": "user", "content": user_prompt_template.format(**user_args)}]
+
+                data = get_batch_line(id=index, messages=prompt, model=model, url=url, options=options, response_format=response_format, json_schema=json_schema)
                 f.write(json.dumps(data, ensure_ascii=False) + '\n')
-    
+
+        print("请求Jsonl文件已存至:", file_out)
         gr.Info(f"请求Jsonl文件已存至: {file_out}")
         return True
     except Exception as e:
+        print(e)
         gr.Error(f"写入Jsonl文件失败: {e}")
         return False
 
 
 # 获取单行 json 数据
-def get_batch_line(id, messages, model, url, options, json_output=False):
+def get_batch_line(id, messages, model, url, options, response_format, json_schema):
     data = {
         'custom_id': f"request-{id}",
         'method': 'POST',
@@ -50,36 +59,50 @@ def get_batch_line(id, messages, model, url, options, json_output=False):
             'messages': messages,
         }
     }
+    if response_format == 'json_object':
+        data['body']['response_format'] = {
+            "type": "json_object"
+        }
+    elif response_format == 'json_schema':
+        data['body']['response_format'] = {
+            "type": "json_schema",
+            "json_schema": json_schema
+        }
     return data
 
 # 批量请求
 def start_batch_job(file_in, client):
-    batch_input_file = client.files.create(
-        file=open(file_in, "rb"),
-        purpose="batch"
-    )
-    print("Batch input file uploaded. File ID:", batch_input_file.id)
-    batch_process = client.batches.create(
-        input_file_id=batch_input_file.id,
-        endpoint="/v1/chat/completions",
-        completion_window="24h",
-        metadata={
-        }
-    )
+    print(file_in)
+    try:
+        batch_input_file = client.files.create(
+            file=open(file_in, "rb"),
+            purpose="batch"
+        )
+        print("Batch input file uploaded. File ID:", batch_input_file.id)
+        batch_process = client.batches.create(
+            input_file_id=batch_input_file.id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h",
+            metadata={
+            }
+        )
 
-    while batch_process.status == "validating":
-        time.sleep(2)
-        batch_process = client.batches.retrieve(batch_process.id)
+        while batch_process.status == "validating":
+            time.sleep(2)
+            batch_process = client.batches.retrieve(batch_process.id)
 
-    if batch_process.status == "failed":
-        gr.Error("批处理请求失败: " + batch_process.error)
-        return None
-    gr.Info(f"批处理请求运行中。")
-    print("Batch process started. Batch ID:", batch_process.id)
-    return batch_process.id
+        if batch_process.status == "failed":
+            gr.Error("批处理请求失败: " + batch_process.error)
+            return None
+        gr.Info(f"批处理请求运行中。")
+        print("Batch process started. Batch ID:", batch_process.id)
+        return batch_process.id
+    except Exception as e:
+        gr.Error(f"批处理请求失败: {e}")
+        return
 
 # 获取批量请求结果
-def get_batch_results(batch_id, file_out,client, progress=gr.Progress()):
+def get_batch_results(batch_id, file_out, client):
     batch = client.batches.retrieve(batch_id)
     while batch.status == "validating":
         time.sleep(2)
@@ -89,77 +112,80 @@ def get_batch_results(batch_id, file_out,client, progress=gr.Progress()):
     current = batch.request_counts.completed
     print("Total requests:", total)
 
+    progress = gr.Progress(track_tqdm=True)
+    progress(current/total, desc="Starting")
     while batch.status != "completed":
         batch = client.batches.retrieve(batch_id)
         if batch.status == "failed":
             print("Batch failed")
             gr.Error("批处理请求失败: " + batch.error)
-            return False
+            return None
  
         # print("Batch process status:", batch.status)
         if batch.request_counts.completed != current:
             current = batch.request_counts.completed
-            progress(current / total)
+            print(f"Completed requests: {current}/{total}")
+            if current == total:
+                progress(0.9, "Processing")
+            else:
+                progress(current / total, "Processing")
         time.sleep(2)
 
     if batch.status == "completed":
         print("Batch process completed.")
-        batch_output_file = client.files.content(batch.output_file_id)
 
         print("Downloading batch output file...")
-        with open(file_out, "w", encoding="utf-8") as f:
-            f.write(batch_output_file.text)
+        if not batch.output_file_id:
+            print("Batch output file not found.")
+            batch_error_file = client.files.content(batch.error_file_id)
+            file_out = file_out.replace(".json", "_error.json")
+            with open(file_out, "w", encoding="utf-8") as f:
+                f.write(batch_error_file.text)
+        else:
+            batch_output_file = client.files.content(batch.output_file_id)
+            with open(file_out, "w", encoding="utf-8") as f:
+                f.write(batch_output_file.text)
+        progress(1, "Finish")
 
+        print("Batch output file saved to", file_out)
         gr.Info(f"批处理结果已存至: {file_out}")
-        return True
+        return file_out
     else:
         print("Batch process not completed yet.")
         gr.Error("批处理请求失败: " + batch.error)
-        return False
+        return None
 
 # Format batch results into a csv file
-def format_batch_results(input_df, batch_result_jsonl, file_out, output_column):
+def format_batch_results(input_df, batch_result_jsonl, file_out, output_column, json_mode=False):
     try:
         df = pd.read_json(batch_result_jsonl, lines=True)
         df_raw = input_df
         df_raw['id'] = np.arange(0, len(df))
 
-        df[output_column] = df['response'].apply( 
-        lambda x: x['body']['choices'][0]['message']['content'])
+        print("Formatting batch results...")
+        if batch_result_jsonl.endswith('_error.jsonl'):
+            df[output_column] = df['response'].apply(lambda x: x['body']['error']['message'])
+        else:
+            df[output_column] = df['response'].apply(lambda x: x['body']['choices'][0]['message']['content'])
+
+            if json_mode:
+                json_keys = json.loads(df.iloc[0][output_column]).keys()
+                for key in json_keys:
+                    df[key] = df[output_column].apply(lambda x: json.loads(x)[key])
+
         df['id'] = df['custom_id'].apply(lambda x: int(x.split('-')[-1]))
+        df.drop(columns=['response'], inplace=True)
         df.drop(columns=['custom_id'], inplace=True)
         df.drop(columns=['error'], inplace=True)
 
         combined_df = pd.merge(df_raw, df, on='id', how='left')
         combined_df.to_excel(file_out, index=False)
+        print("Data saved to", file_out)
     except Exception as e:
+        print("batch_formatting_error:", e)
         gr.Error(f"写入Excel文件失败: {e}")
         return False
 
     print("Data saved to", file_out)
     gr.Info(f"批处理任务已完成, 数据已存至: {file_out}")
     return True
-
-
-if __name__ == "__main__":
-    # MODEL = 'gpt-4o'
-    MODEL = "ft:gpt-4o-mini-2024-07-18:personal::9yGNDJOv"
-    TEMPERATURE = 0.3
-    URL = '/v1/chat/completions'
-
-    file_in = "data/train_c1_spark_raw.xlsx"
-    file_out = "SFT/0820/train_c1_spark_raw.csv"
-
-    file_name = file_in.split('/')[-1].split('.')[0]
-
-    input_jsonl_file = "output/metas/" + file_name + '_batch.jsonl'
-    output_jsonl_file = "output/metas/" + file_name + '_batch_output.jsonl'
-    
-    to_batch_jsonl(file_in, input_jsonl_file, get_prompt_ai_reply, MODEL, URL, TEMPERATURE, json_mode=False)
-    batch_id = start_batch_job(input_jsonl_file)
-    print("Batch ID:", batch_id)
-
-    # batch_id = "batch_2AZz1v5tW4Vfb7H2IWJpyFsQ"
-    time.sleep(1)
-    get_batch_results(batch_id, output_jsonl_file)
-    format_batch_results(file_in, output_jsonl_file, file_out)
